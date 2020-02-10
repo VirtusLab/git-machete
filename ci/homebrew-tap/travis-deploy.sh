757fbd4d3c112ab3d49e2387e09ee1fef7701a6c
Pawel Lipski <pawel.p.lipski@gmail.com>
2020-02-10 16:53:52 +0100

    Open a version-bump PR to NixOS/nixpkgs automatically on each release (#79)

--- ci/homebrew-tap/travis-deploy.sh
+++ ci/homebrew-tap/travis-deploy.sh
@@ -9,7 +9,7 @@ git config user.email "travis@travis-ci.org"
 git config user.name "Travis CI"
 [[ -n $TRAVIS_TAG ]]
 VERSION=${TRAVIS_TAG#v}
-sha256=$(curl -s https://pypi.org/pypi/git-machete/$VERSION/json  | jq --raw-output '.urls | map(select(.packagetype == "sdist")) | .[0].digests.sha256')
+sha256=$(curl -s https://pypi.org/pypi/git-machete/$VERSION/json | jq --raw-output '.urls | map(select(.packagetype == "sdist")) | .[0].digests.sha256')
 sed -i "s/git-machete-.*\.tar\.gz/git-machete-$VERSION.tar.gz/" git-machete.rb
 sed -i "s/^  sha256 .*/  sha256 \"$sha256\"/" git-machete.rb
 git add git-machete.rb
