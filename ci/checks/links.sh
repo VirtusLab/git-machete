ad1d72990bf762ee1c23df237b19591cd5c7fb5a
Pawel Lipski <pawel.p.lipski@gmail.com>
2020-07-09 18:18:42 +0200

    Fix HTTP 405 Method Not Allowed error in ci/checks/links.sh

--- ci/checks/links.sh
+++ ci/checks/links.sh
@@ -5,7 +5,11 @@
 
 set -e -o pipefail -u
 
-git ls-files ':!ci/' ':!hook_samples/' | xargs grep -ho "https://[^]') ]*" | sort -u | xargs -t -l curl -LfsI --max-time 30 -o/dev/null -w "> %{http_code}\n" || {
+# Not passing `-I`/`--head` flag to `curl` since medium.com sometimes responds with 405 Method Not Allowed (sometimes with 200 OK, though).
+git ls-files ':!ci/' ':!hook_samples/' \
+| xargs grep -ho "https://[^]') ]*" \
+| sort -u \
+| xargs -t -l curl -Lfs --max-time 30 -o/dev/null -w "> %{http_code}\n" || {
   echo "Some of the links found in the codebase did not return 2xx HTTP status, please fix"
   exit 1
 }
