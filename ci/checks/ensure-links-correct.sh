f64f2efb9362ee358bb6f5c85517cedfef2bc014
Pawel Lipski <pawel.p.lipski@gmail.com>
2020-12-08 20:49:09 +0100

    Improve validation of links

---
+++ ci/checks/ensure-links-correct.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -e -o pipefail -u
+
+# Downtime of the linked websites shouldn't block a release.
+if [[ ${TRAVIS_BRANCH-} != master ]]; then
+  extra_options='--use=lint-no-dead-urls'
+fi
+
+remark --use=validate-links $extra_options --ignore-path=.gitignore  --frail .
