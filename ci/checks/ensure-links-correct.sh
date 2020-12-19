0258b415f369c42bda0c876dc49ff5403e15d34c
Pawel Lipski <pawel.p.lipski@gmail.com>
2020-12-19 20:25:20 +0100

    Fix ensure-links-correct.sh for master builds

--- ci/checks/ensure-links-correct.sh
+++ ci/checks/ensure-links-correct.sh
@@ -5,6 +5,8 @@ set -e -o pipefail -u
 # Downtime of the linked websites shouldn't block a release.
 if [[ ${TRAVIS_BRANCH-} != master ]]; then
   extra_options='--use=lint-no-dead-urls'
+else
+  extra_options=''
 fi
 
 remark --use=validate-links $extra_options --ignore-path=.gitignore  --frail .
