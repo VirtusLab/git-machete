46684515c90e2c045f501a44b8db639f991b9fd4
Alex Ford <alex.ford@abcellera.com>
2021-07-02 09:00:45 -0700

    Tidy maxDiff in test setup.

--- git_machete/tests/functional/test_machete.py
+++ git_machete/tests/functional/test_machete.py
@@ -86,6 +86,10 @@ class MacheteTester(unittest.TestCase):
         self.assertEqual(self.launch_command(*cmd), self.adapt(expected_result))
 
     def setUp(self) -> None:
+        # Status diffs can be quite large, default to ~256 lines of diff context
+        # https://docs.python.org/3/library/unittest.html#unittest.TestCase.maxDiff
+        self.maxDiff = 80 * 256
+
         self.setup = SandboxSetup()
 
         (
@@ -179,7 +183,6 @@ class MacheteTester(unittest.TestCase):
         )
 
     def test_traverse_no_push(self) -> None:
-        self.maxDiff = None
         self.setup_discover_standard_tree()
 
         self.launch_command("traverse", "-Wy", "--no-push")
@@ -214,7 +217,6 @@ class MacheteTester(unittest.TestCase):
         )
 
     def test_traverse_no_push_override(self) -> None:
-        self.maxDiff = None
         self.setup_discover_standard_tree()
 
         self.launch_command("traverse", "-Wy", "--no-push", "--push")
@@ -249,7 +251,6 @@ class MacheteTester(unittest.TestCase):
         )
 
     def test_traverse_no_push_untracked(self) -> None:
-        self.maxDiff = None
         self.setup_discover_standard_tree()
 
         self.launch_command("traverse", "-Wy", "--no-push-untracked")
@@ -502,7 +503,6 @@ class MacheteTester(unittest.TestCase):
         )
 
     def test_squash_merge(self) -> None:
-        self.maxDiff = None
         repo = (
             self.setup.new_branch("root")
             .commit("root")
