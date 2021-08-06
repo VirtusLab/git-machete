2c2c62049600912fe0f621b0ba634ccba51f1c63
rifel123 <piotr.ksel@gmail.com>
2021-08-06 08:23:33 +0200

    introduce ensure-version-bumped.sh

---
+++ ci/checks/ensure-version-bumped.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e -o pipefail -u
+
+exit_code=0
+
+last_tag=$(git describe --tags --abbrev=0)
+last_tag_version=${last_tag:1} 
+current_version=$(grep '__version__ = ' ../../git_machete/__init__.py | cut -d\' -f2)
+
+if [[ "$(printf "$current_version\n$last_tag_version" | sort -V -r | head -n 1)" == "$last_tag_version" ]]; then
+  echo "Please Increment version with new release! Last released version: $last_tag_version"
+  exit_code=1
+fi
+
+exit $exit_code
