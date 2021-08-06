c06b9684692ec870abbcda3596a32c8fd00305fc
rifel123 <piotr.ksel@gmail.com>
2021-08-06 10:32:08 +0200

    fix typo, expand --version-sort command

--- ci/checks/ensure-version-bumped.sh
+++ ci/checks/ensure-version-bumped.sh
@@ -2,15 +2,11 @@
 
 set -e -o pipefail -u
 
-exit_code=0
-
 last_tag=$(git describe --tags --abbrev=0)
-last_tag_version=${last_tag:1}
+last_tag_version=${last_tag/v/}
 current_version=$(grep '__version__ = ' git_machete/__init__.py | cut -d\' -f2)
 
-if [[ "$(printf "$current_version\n$last_tag_version" | sort -V -r | head -n 1)" == "$last_tag_version" ]]; then
-  echo "Please Increment version with new release! Last released version: $last_tag_version"
-  exit_code=1
+if [[ "$(printf "$current_version\n$last_tag_version" | sort --version-sort | tail -n 1)" == "$last_tag_version" ]]; then
+  echo "Please increment version with new release! Last released version: $last_tag_version"
+  exit 1
 fi
-
-exit $exit_code
