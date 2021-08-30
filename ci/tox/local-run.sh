b7b8ae97e8dc27795983602377774bf296273f69
Piotr Ksel <73847352+rifel123@users.noreply.github.com>
2021-08-30 14:46:36 +0200

    call export_directory_hash instead of export_hash in local_run scripts. (#197)
    * call export_directory_hash instead of export_hash in local_run scripts.
    
    * add setting envs for MacOS users in export-gpg-ssh-config
    
    * put wrap0_flag

--- ci/tox/local-run.sh
+++ ci/tox/local-run.sh
@@ -4,7 +4,7 @@ set -e -o pipefail -u
 
 source ../local-run-commons.sh tox
 
-export_hash tox
+export_directory_hash tox
 cd "$(git rev-parse --show-toplevel)"/ci/tox/
 check_var GIT_VERSION
 check_var PYTHON_VERSION
