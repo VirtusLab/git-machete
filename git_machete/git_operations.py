76403d1a9442e5ba20c66993f0bb9f3202484c65
rifel123 <piotr.ksel@gmail.com>
2021-09-08 09:04:42 +0200

    get remote git url from config

--- git_machete/git_operations.py
+++ git_machete/git_operations.py
@@ -161,7 +161,7 @@ class GitContext:
         return self.__remotes_cached
 
     def get_url_of_remote(self, remote: str) -> str:
-        return self.popen_git("remote", "get-url", "--", remote).strip()
+        return self.popen_git("config", "--get", f"remote.{remote}.url").strip()
 
     def fetch_remote(self, remote: str) -> None:
         if remote not in self.__fetch_done_for:
