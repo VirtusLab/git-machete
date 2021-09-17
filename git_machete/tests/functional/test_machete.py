466bb791dc34572d6e5cf3b521e46a256569a2cb
rifel123 <piotr.ksel@gmail.com>
2021-09-17 16:26:14 +0200

    clear __remotes_cached with flush_caches

--- git_machete/tests/functional/test_machete.py
+++ git_machete/tests/functional/test_machete.py
@@ -1592,7 +1592,6 @@ class MacheteTester(unittest.TestCase):
                 .push()
                 .check_out('feature')
                 .add_remote('new_origin', 'https://github.com/user/repo.git')
-                .sleep(1)
         )
 
         self.launch_command("discover", "-y")
@@ -1647,7 +1646,6 @@ class MacheteTester(unittest.TestCase):
                 .reset_to("ignore-trailing@{1}")
                 .delete_branch("root")
                 .add_remote('new_origin', 'https://github.com/user/repo.git')
-                .sleep(1)
         )
         self.launch_command("discover", "-y")
         self.launch_command('github', 'anno-prs')
@@ -1721,7 +1719,6 @@ class MacheteTester(unittest.TestCase):
                 .commit("remove outdated fields")
                 .check_out("call-ws")
                 .add_remote('new_origin', 'https://github.com/user/repo.git')
-                .sleep(1)
         )
 
         self.launch_command("discover")
