87375de37d1f3f1d8a593d1de0f2face04d8dba7
rifel123 <piotr.ksel@gmail.com>
2021-09-28 12:27:40 +0200

    add hint about shebang, search for python files in git_machete directory

--- ci/checks/enforce-correct-shebangs.sh
+++ ci/checks/enforce-correct-shebangs.sh
@@ -2,7 +2,7 @@
 
 set -e -o pipefail -u
 
-if grep -r -e '^#!\/usr\/bin\/env python$' -e '^#!\/usr\/bin\/env python2$' ../../git_machete; then
-  echo "Ambigous python shebang"
+if grep -r -e '^#!/usr/bin/env python$' -e '^#!/usr/bin/env python2$' git_machete; then
+  echo "Ambigouos python shebang, please declare shebangs that point to python3:  #!/usr/bin/env python3"
   exit 1
 fi
