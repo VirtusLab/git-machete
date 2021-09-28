d242393981f0aad8a4c8a4185ae9601de932841c
rifel123 <piotr.ksel@gmail.com>
2021-09-28 14:32:06 +0200

    first round of fixes

--- ci/checks/enforce-correct-shebangs.sh
+++ ci/checks/enforce-correct-shebangs.sh
@@ -2,7 +2,7 @@
 
 set -e -o pipefail -u
 
-if grep -r -e '^#!/usr/bin/env python$' -e '^#!/usr/bin/env python2$' git_machete; then
-  echo "Ambigouos python shebang, please declare shebangs that point to python3:  #!/usr/bin/env python3"
+if grep -r '^#!/usr/bin/env python2\?$' .; then
+  echo "Ambiguous python shebang, please declare shebangs that point to python3:  #!/usr/bin/env python3"
   exit 1
 fi
