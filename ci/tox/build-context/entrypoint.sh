ac4fb708e6fead71ffdbda92cdc4539122d477fa
Piotr Ksel <73847352+rifel123@users.noreply.github.com>
2021-10-04 12:36:18 +0200

    Check PEP8 correctness for one python version in CI (#262)
    * run PEP8 checks only for one python version
    
    * add git versions in job names on CI
    
    * remove dots from jobs names
    
    * split git version in job names for clarity
    
    * change job names, change env variable name for PEP8 checks
    
    * fix typo
    
    * add empty line to separate code blocks
    
    * correct PEP8 env variable name in comments

--- ci/tox/build-context/entrypoint.sh
+++ ci/tox/build-context/entrypoint.sh
@@ -8,9 +8,13 @@
 set -e -u -x
 
 if [[ $CHECK_COVERAGE = true ]]; then
-  TOX_ENV_LIST="pep8,mypy-py${PYTHON_VERSION/./},coverage"
+  TOX_ENV_LIST="mypy-py${PYTHON_VERSION/./},coverage"
 else
-  TOX_ENV_LIST="pep8,mypy-py${PYTHON_VERSION/./},py${PYTHON_VERSION/./}"
+  TOX_ENV_LIST="mypy-py${PYTHON_VERSION/./},py${PYTHON_VERSION/./}"
+fi
+
+if [[ $PEP8 = true ]]; then
+  TOX_ENV_LIST="$TOX_ENV_LIST,pep8"
 fi
 
 if [[ $BUILD_DOCS = true ]]; then
