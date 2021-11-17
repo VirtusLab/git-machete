d251e405125f2a28c5c96bc20dece33ccf14af66
rifel123 <piotr.ksel@gmail.com>
2021-11-17 10:38:05 +0100

    change additional_msg body

--- git_machete/github.py
+++ git_machete/github.py
@@ -130,7 +130,7 @@ def __extract_failure_info_from_422(response: Any) -> str:
     if response['message'] != 'Validation Failed':
         if 'Reviews may only be requested from collaborators.' in response['message']:
             additional_msg = "\nThere are some invalid reviewers in .git/info/reviewers file. " \
-                             "Please remove them or all file to create a pull request without reviewers."
+                             "Please remove them or remove the entire file to create a pull request without reviewers."
         return str(response['message']) + additional_msg
     ret: List[str] = []
     if response.get('errors'):
