931fd2c58aaebdc325e1fd2c9ceacecee80f72e7
Piotr Ksel <73847352+rifel123@users.noreply.github.com>
2021-11-17 11:04:49 +0100

    Issue #334: install git-machete locally from freshly generated brew formula  (#341)
    * add installation of git-machete from local homebrew formula
    
    * remove curl installation
    
    * add sudo to eval instructions
    
    * change the output file for brew
    
    * remove sudos
    
    * remove double brew install
    
    * change url of brew source to test.pypi.io
    
    * remove testing jobs
    
    * restore testing job, add version check and git-machete removal
    
    * put exit 2
    
    * remove duplicated sed
    
    * remove testing job

--- ci/homebrew-tap/ci-deploy.sh
+++ ci/homebrew-tap/ci-deploy.sh
@@ -21,6 +21,7 @@ sha256=$(
   curl -s https://$pypi_host/pypi/git-machete/$version/json \
   | jq --raw-output '.urls | map(select(.packagetype == "sdist")) | .[0].digests.sha256')
 sed -i "s/git-machete-.*\.tar\.gz/git-machete-$version.tar.gz/" git-machete.rb
+sed -i "s/pypi\.io/$pypi_host/" git-machete.rb
 sed -i "s/^  sha256 .*/  sha256 \"$sha256\"/" git-machete.rb
 git add git-machete.rb
 git commit --message "Release $version, CircleCI build: $CIRCLE_BUILD_NUM"
@@ -29,4 +30,14 @@ if [[ $do_push == true ]]; then
   git push origin master
 else
   echo "Refraining from push since it's a dry run"
+  # install git-machete from local formula with homebrew
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" < /dev/null
+  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+  brew install ./git-machete.rb
+  if [[ "$version" != "$(git machete --version | cut -d' ' -f4)" ]]; then
+    echo "Something went wrong during brew installation: installed version does not match version from formula."
+    echo "Formula version: $version, installed version: $(git machete --version | cut -d' ' -f4)"
+    exit 1
+  fi
+  brew remove git-machete
 fi
