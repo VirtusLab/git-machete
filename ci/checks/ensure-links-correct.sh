2433c13955b73ff0a07e0d61eeca8877d9b61ce4
Andrzej Malota <andrzej.malota@tesco.com>
2022-02-02 13:18:51 +0100

    init

--- ci/checks/ensure-links-correct.sh
+++ ci/checks/ensure-links-correct.sh
@@ -2,11 +2,4 @@
 
 set -e -o pipefail -u
 
-# Downtime of the linked websites shouldn't block a release.
-if [[ ${CIRCLE_BRANCH-} != master ]]; then
-  extra_options='--use=lint-no-dead-urls'
-else
-  extra_options=''
-fi
-
-remark --use=validate-links $extra_options --ignore-path=.gitignore  --frail .
+remark --ignore-path=.gitignore  --frail .
