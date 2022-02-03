4617f6a05c96d3b684fa427aa6442129584a6f98
Andrzej Malota <andrzej.malota@tesco.com>
2022-02-03 10:45:40 +0100

    add config file for master

--- ci/checks/ensure-links-correct.sh
+++ ci/checks/ensure-links-correct.sh
@@ -2,4 +2,11 @@
 
 set -e -o pipefail -u
 
-remark --ignore-path=.gitignore  --frail .
+# Downtime of the linked websites shouldn't block a release.
+if [[ ${CIRCLE_BRANCH-} != master ]]; then
+  extra_options='--rc-path=.remarkrc.yml'
+else
+  extra_options='--rc-path=.remarkrc_master.yml'
+fi
+
+remark $extra_options --ignore-path=.gitignore  --frail .
