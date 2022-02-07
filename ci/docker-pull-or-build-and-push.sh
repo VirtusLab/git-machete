939fc4496ffbd557ee64963accedb28ed95651f7
Andrzej Malota <andrzej.malota@tesco.com>
2022-02-07 19:11:17 +0100

    cd

--- ci/docker-pull-or-build-and-push.sh
+++ ci/docker-pull-or-build-and-push.sh
@@ -7,13 +7,6 @@ DIRECTORY_HASH=$(git rev-parse HEAD:ci/$image_name)
 export DIRECTORY_HASH
 cd ci/$image_name/
 
-USER_ID=3000
-export USER_ID
-GROUP_ID=3000
-export GROUP_ID
-sudo groupadd -g "$USER_ID" ci_user
-sudo useradd ci_user -u "$USER_ID" -g "$GROUP_ID" -s /bin/bash
-
 function retry() {
   attempts=$1
   interval=10
@@ -34,11 +27,18 @@ function retry() {
 # (`error pulling image configuration: Get "https://docker-images-prod.s3.dualstack.us-east-1.amazonaws.com/...": dial tcp ...:443: i/o timeout`)
 retry 3 docker-compose --ansi never pull $image_name
 
+USER_ID=3000
+export USER_ID
+GROUP_ID=3000
+export GROUP_ID
+sudo groupadd -g "$USER_ID" ci_user
+sudo useradd ci_user -u "$USER_ID" -g "$GROUP_ID" -s /bin/bash
+
 # A very unpleasant workaround for https://github.com/docker/compose/issues/7258
 # (since v1.25.1, `docker-compose pull` is NOT failing when it can't fetch the image).
 image_tag=$(docker-compose --ansi never config | yq eval ".services.$image_name.image" -)
 docker image inspect "$image_tag" &>/dev/null || {
-  docker-compose --ansi never build --build-arg user_id="$USER_ID" --build-arg group_id="$GROUP_ID" $image_name
+  docker-compose --ansi never build $image_name
   # In builds coming from forks, secret vars are unavailable for security reasons; hence, we have to skip pushing the newly built image.
   if [[ ${DOCKER_PASSWORD-} && ${DOCKER_USERNAME-} ]]; then
     echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
