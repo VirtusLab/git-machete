0ba9805bd567282887818289266d62bfc6049e93
Andrzej Malota <andrzej.malota@tesco.com>
2022-02-08 10:35:33 +0100

    new

--- ci/docker-pull-or-build-and-push.sh
+++ ci/docker-pull-or-build-and-push.sh
@@ -32,21 +32,24 @@ export DOCKER_USER="3000:3000"
 
 # Let's retry pulling the image in case of a spurious failure
 # (`error pulling image configuration: Get "https://docker-images-prod.s3.dualstack.us-east-1.amazonaws.com/...": dial tcp ...:443: i/o timeout`)
-retry 3 docker-compose --ansi never pull $image_name
+retry 3 GID="3000" UID="3000" docker-compose --ansi never pull $image_name
 
-
-#sudo groupadd -g "UID" ci_user
-#sudo useradd ci_user -u "UID" -g "GID" -m -s /bin/bash
+USER_ID=3000
+export USER_ID
+GROUP_ID=3000
+export GROUP_ID
+sudo groupadd -g "$USER_ID" ci_user
+sudo useradd ci_user -u "$USER_ID" -g "$GROUP_ID" -m -s /bin/bash
 
 # A very unpleasant workaround for https://github.com/docker/compose/issues/7258
 # (since v1.25.1, `docker-compose pull` is NOT failing when it can't fetch the image).
 image_tag=$(docker-compose --ansi never config | yq eval ".services.$image_name.image" -)
 docker image inspect "$image_tag" &>/dev/null || {
-  docker-compose --ansi never build $image_name
+   GID="3000" UID="3000" docker-compose --ansi never build $image_name
   # In builds coming from forks, secret vars are unavailable for security reasons; hence, we have to skip pushing the newly built image.
   if [[ ${DOCKER_PASSWORD-} && ${DOCKER_USERNAME-} ]]; then
     echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
     # In case the push fails due to e.g. timeouts (which unfortunately sometimes happen on CI), we don't want to fail the entire deployment.
-    docker-compose --ansi never push $image_name || true
+     GID="3000" UID="3000" docker-compose --ansi never push $image_name || true
   fi
 }
