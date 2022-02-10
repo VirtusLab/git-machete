5fd9457b444c2fe694d94c1cc3804e3cbf312922
Andrzej Małota <86779115+amalota@users.noreply.github.com>
2022-02-10 15:37:45 +0100

    Issue #396: (Option #3) Use root user in the container, non-root locally (#422)

--- ci/rpm/local-run.sh
+++ ci/rpm/local-run.sh
@@ -7,5 +7,7 @@ export_directory_hash rpm
 cd "$(git rev-parse --show-toplevel)"/ci/rpm/
 
 set -x
+export TARGET=local
+export MOUNT_POINT=/home/ci-user
 docker-compose build --build-arg user_id="$(id -u)" --build-arg group_id="$(id -g)" rpm
 docker-compose run rpm
