839631fa6f9be2a94014ae395446f3afb8f5c60e
Andrzej Malota <andrzej.malota@tesco.com>
2022-03-03 17:32:31 +0100

    cd

--- ci/snap/ci-deploy.sh
+++ ci/snap/ci-deploy.sh
@@ -5,19 +5,19 @@ set -e -o pipefail -u -x
 sudo apt-get update
 sudo apt-get install -y snapd
 sudo snap install snapcraft --classic
-#sudo lxd init --minimal
-## `--use-lxd` applied to use a LXD container instead of a VM, to work around lack of support for KVM on CircleCI VMs.
-#snapcraft --use-lxd
+sudo lxd init --minimal
+# `--use-lxd` applied to use a LXD container instead of a VM, to work around lack of support for KVM on CircleCI VMs.
+snapcraft --use-lxd
 
 if [[ ${1-} == "--dry-run" || ${CIRCLE_BRANCH-} != "master" ]]; then
-#  ! command -v git-machete
-#  sudo snap install git-machete_*.snap --dangerous --classic
-#  git machete version
-#  sudo snap remove git-machete
+  ! command -v git-machete2
+  sudo snap install git-machete*.snap --dangerous --classic
+  git machete version
+  sudo snap remove git-machete2
   echo "MASTER"
   echo "$SNAPCRAFT_LOGIN_CREDENTIALS_CONTENTS_BASE64" | base64 -d > ~/.snapcraft.login
   snapcraft login --with ~/.snapcraft.login
-  snapcraft status git-machete
+  snapcraft status git-machete2
 else
   echo "MASTER"
   echo "$SNAPCRAFT_LOGIN_CREDENTIALS_CONTENTS_BASE64" | base64 -d > ~/.snapcraft.login
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(this_directory, 'README.md')) as f:
     long_description = f.read()
 
 setup(
-    name='git-machete2',
+    name='git-machete',
     version=__version__,
     description='Probably the sharpest git repository organizer & rebase/merge workflow automation tool you\'ve ever seen',
     long_description=long_description,
