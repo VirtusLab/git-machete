e1b6c4a50c83369c2467228813f16b23e032f769
Andrzej Malota <andrzej.malota@tesco.com>
2022-03-04 12:14:51 +0100

    cd

--- ci/snap/ci-deploy.sh
+++ ci/snap/ci-deploy.sh
@@ -4,16 +4,17 @@ set -e -o pipefail -u
 
 sudo apt-get update
 sudo apt-get install -y snapd
+sudo snap install review-tools
 sudo snap install snapcraft --classic
 sudo lxd init --minimal
 # `--use-lxd` applied to use a LXD container instead of a VM, to work around lack of support for KVM on CircleCI VMs.
 snapcraft --use-lxd
 
 if [[ ${1-} == "--dry-run" || ${CIRCLE_BRANCH-} != "master" ]]; then
-  ! command -v git-machete
-  sudo snap install git-machete*.snap --dangerous --classic
-  git machete version
-  sudo snap remove git-machete
+#  ! command -v git-machete
+#  sudo snap install git-machete*.snap --dangerous --classic
+#  git machete version
+#  sudo snap remove git-machete
   echo "MASTER"
   echo "$SNAPCRAFT_LOGIN_CREDENTIALS_CONTENTS_BASE64" base64 -d > ~/.snapcraft.login
   snapcraft login --with ~/.snapcraft.login
