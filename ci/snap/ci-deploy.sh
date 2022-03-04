52259f43fd91740cdb01bbba9ae510e87d1f542a
Andrzej Malota <andrzej.malota@tesco.com>
2022-03-04 13:53:33 +0100

    cd

--- ci/snap/ci-deploy.sh
+++ ci/snap/ci-deploy.sh
@@ -5,32 +5,21 @@ set -e -o pipefail -u
 sudo apt-get update
 sudo apt-get install -y snapd
 sudo snap install review-tools
-#sudo apt-get install --reinstall resolvconf
-#sudo snap install multipass --classic
-#sudo snap restart multipass.multipassd
-#sudo chmod a+w /var/snap/multipass/common/multipass_socket
 sudo snap install snapcraft --edge --classic
 sudo lxd init --minimal
 # `--use-lxd` applied to use a LXD container instead of a VM, to work around lack of support for KVM on CircleCI VMs.
 snapcraft --use-lxd
-#snapcraft
-#docker run -v $(pwd):$(pwd) -t ubuntu:xenial sh -c "apt update -qq && apt install snapcraft -y && cd $(pwd) && snapcraft"
 
 if [[ ${1-} == "--dry-run" || ${CIRCLE_BRANCH-} != "master" ]]; then
-  ! command -v git-machete3
+  echo "DEVELOP"
+  ! command -v git-machete2
   sudo snap install git-machete*.snap --dangerous --classic
-  git machete3 version
-  sudo snap remove git-machete3
+  git machete2 version
+  sudo snap remove git-machete2
+else
   echo "MASTER"
   echo "$SNAPCRAFT_LOGIN_CREDENTIALS_CONTENTS_BASE64" base64 -d > ~/.snapcraft.login
   snapcraft login --with ~/.snapcraft.login
-#  snapcraft register git-machete3 --yes
-#  snapcraft push git-machete*.snap
   snapcraft upload --release=stable git-machete*.snap
-  snapcraft status git-machete3
-else
-  echo "MASTER"
-  echo "$SNAPCRAFT_LOGIN_CREDENTIALS_CONTENTS_BASE64" | base64 -d > ~/.snapcraft.login
-  snapcraft login --with ~/.snapcraft.login
-  snapcraft upload --release=stable git-machete*.snap
+  snapcraft status git-machete2
 fi
