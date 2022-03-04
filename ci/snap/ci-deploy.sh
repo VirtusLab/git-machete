09076ea37e0df3884edd5c6651e63526fa482c9e
Andrzej Malota <andrzej.malota@tesco.com>
2022-03-04 13:31:15 +0100

    cd

--- ci/snap/ci-deploy.sh
+++ ci/snap/ci-deploy.sh
@@ -4,7 +4,7 @@ set -e -o pipefail -u
 
 sudo apt-get update
 sudo apt-get install -y snapd
-#sudo snap install review-tools
+sudo snap install review-tools
 #sudo apt-get install --reinstall resolvconf
 #sudo snap install multipass --classic
 #sudo snap restart multipass.multipassd
@@ -17,16 +17,16 @@ snapcraft --use-lxd
 #docker run -v $(pwd):$(pwd) -t ubuntu:xenial sh -c "apt update -qq && apt install snapcraft -y && cd $(pwd) && snapcraft"
 
 if [[ ${1-} == "--dry-run" || ${CIRCLE_BRANCH-} != "master" ]]; then
-#  ! command -v git-machete
-#  sudo snap install git-machete*.snap --dangerous --classic
-#  git machete version
-#  sudo snap remove git-machete
+  ! command -v git-machete
+  sudo snap install git-machete*.snap --dangerous --classic
+  git machete version
+  sudo snap remove git-machete
   echo "MASTER"
   echo "$SNAPCRAFT_LOGIN_CREDENTIALS_CONTENTS_BASE64" base64 -d > ~/.snapcraft.login
   snapcraft login --with ~/.snapcraft.login
 #  snapcraft register git-machete3 --yes
-  snapcraft push git-machete*.snap
-  snapcraft upload --release=edge git-machete*.snap
+#  snapcraft push git-machete*.snap
+  snapcraft upload --release=stable git-machete*.snap
   snapcraft status git-machete3
 else
   echo "MASTER"
