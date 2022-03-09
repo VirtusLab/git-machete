e9ec115ab8d215b9fea29c919e5f3173ecace49a
Andrzej Małota <86779115+amalota@users.noreply.github.com>
2022-03-09 10:04:47 +0100

    Issue #461: Extract version wherever possible with python3 setup.py --version (#464)

--- ci/checks/ensure-version-bumped.sh
+++ ci/checks/ensure-version-bumped.sh
@@ -4,7 +4,7 @@ set -e -o pipefail -u
 
 last_tag=$(git describe --tags --abbrev=0)
 last_tag_version=${last_tag/v/}
-current_version=$(grep '__version__ = ' git_machete/__init__.py | cut -d\' -f2)
+current_version=$(python3 setup.py --version)
 
 if [[ "$(printf "$current_version\n$last_tag_version" | sort --version-sort | tail -n 1)" == "$last_tag_version" ]]; then
   echo "Please increment version with new release! Last released version: $last_tag_version"
