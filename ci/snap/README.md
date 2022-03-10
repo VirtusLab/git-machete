33f48b19caef3f66f4b3155627b7a91613dd8301
Andrzej Małota <86779115+amalota@users.noreply.github.com>
2022-03-10 14:15:25 +0100

    Issue #436: Automate release of freshly-built snap to latest/stable channel (#453)

--- ci/snap/README.md
+++ ci/snap/README.md
@@ -1,3 +1,1 @@
 Note that this directory is distinct from top-level [snap/](https://github.com/VirtusLab/git-machete/tree/master/snap) directory.
-This directory only contains a script that triggers a dry-run of snap builds,
-and is **not** used when actually building the snap.
---
+++ ci/snap/ci-deploy.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -e -o pipefail -u
+
+sudo apt-get update
+sudo apt-get install -y snapd
+sudo snap install review-tools
+sudo snap install snapcraft --classic
+sudo lxd init --minimal
+# `--use-lxd` applied to use a LXD container instead of a VM, to work around lack of support for KVM on CircleCI VMs.
+snapcraft --use-lxd
+
+if [[ ${1-} == "--dry-run" || ${CIRCLE_BRANCH-} != "master" ]]; then
+  ! command -v git-machete
+  sudo snap install git-machete*.snap --dangerous --classic
+  git machete version
+  sudo snap remove git-machete
+else
+  echo "$SNAPCRAFT_LOGIN_CREDENTIALS_CONTENTS_BASE64" | base64 -d > ~/.snapcraft.login
+  snapcraft login --with ~/.snapcraft.login
+  snapcraft upload --release=stable git-machete*.snap
+  snapcraft status git-machete
+fi
