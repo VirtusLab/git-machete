ab2d6f9e05b5777f4ef4f7db8e0b1dae40487b1e
Andrzej Malota <andrzej.malota@tesco.com>
2022-04-05 13:47:28 +0100

    init

--- git_machete/git_operations.py
+++ git_machete/git_operations.py
@@ -581,7 +581,7 @@ class GitContext:
             return self.__reflogs_cached[branch]
 
     def create_branch(self, branch: LocalBranchShortName, out_of_revision: AnyRevision) -> None:
-        self._run_git("checkout", "-b", branch, out_of_revision)
+        self._run_git("branch", branch, out_of_revision)
         self.flush_caches()  # the repository state has changed because of a successful branch creation, let's defensively flush all the caches
 
     def flush_caches(self) -> None:
