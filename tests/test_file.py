79487b62a36aaee5fbad5d7f97b667bb8ae99e89
Andrzej Malota <andrzej.malota@tesco.com>
2022-06-14 15:26:44 +0200

    cd

--- tests/test_file.py
+++ tests/test_file.py
@@ -43,6 +43,7 @@ class TestFile:
         assert definition_file_path_relative_to_git_dir == '.git/machete'
 
         # check git machete definition file path when inside worktree
+        self.repo_sandbox.execute("rm -rf test_worktree")
         self.repo_sandbox.add_git_config_key('machete.worktree.useTopLevelMacheteFile', 'false')
         self.repo_sandbox.execute("git worktree add -b new_feature test_worktree develop")
         os.chdir('test_worktree')
