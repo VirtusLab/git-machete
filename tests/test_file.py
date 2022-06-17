9c5d5f179228f7c7c141cd3d31f31468ee0fb083
Andrzej Małota <86779115+amalota@users.noreply.github.com>
2022-06-17 11:57:52 +0200

    Change the default logic for inferring git machete definition file path to what is was before (#551)

--- tests/test_file.py
+++ tests/test_file.py
@@ -41,12 +41,21 @@ class TestFile:
         definition_file_path_relative_to_git_dir = '/'.join(definition_file_path[-2:]).rstrip('\n')
         assert definition_file_path_relative_to_git_dir == '.git/machete'
 
-        # check git machete definition file path when inside a worktree
         if GitContext().get_git_version() >= (2, 5):  # `git worktree` command was introduced in git version 2.5
+            # check git machete definition file path when inside a worktree using the default `True` value
+            # for the `machete.worktree.useTopLevelMacheteFile` key
+            self.repo_sandbox.execute("git worktree add -f -b snickers_feature snickers_worktree develop")
+            os.chdir('snickers_worktree')
+            definition_file_full_path = launch_command("file")
+            definition_file_path = Path(definition_file_full_path).parts
+            definition_file_path_relative_to_git_dir = '/'.join(definition_file_path[-2:]).rstrip('\n')
+            assert definition_file_path_relative_to_git_dir == '.git/machete'
+
+            # check git machete definition file path when inside a worktree but with the `machete.worktree.useTopLevelMacheteFile` key set to `False`
             self.repo_sandbox.add_git_config_key('machete.worktree.useTopLevelMacheteFile', 'false')
-            self.repo_sandbox.execute("git worktree add -f -b new_feature test_worktree develop")
-            os.chdir('test_worktree')
+            self.repo_sandbox.execute("git worktree add -f -b mars_feature mars_worktree develop")
+            os.chdir('mars_worktree')
             definition_file_full_path = launch_command("file")
             definition_file_path = Path(definition_file_full_path).parts
             definition_file_path_relative_to_git_dir = '/'.join(definition_file_path[-4:]).rstrip('\n')
-            assert definition_file_path_relative_to_git_dir == '.git/worktrees/test_worktree/machete'
+            assert definition_file_path_relative_to_git_dir == '.git/worktrees/mars_worktree/machete'
