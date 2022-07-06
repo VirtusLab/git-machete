33522e95ec561a8ab88b6d10bce94796d59fafd1
Andrzej Malota <andrzej.malota@tesco.com>
2022-07-06 15:24:11 +0200

    cd

--- tests/test_status.py
+++ tests/test_status.py
@@ -268,14 +268,14 @@ class TestStatus:
         launch_command('discover', '-y')
         expected_status_output = (
 """  master
-  | 
+  |
   o-bar
-    | 
+    |
     o-foo (untracked)
-      | 
+      |
       o-snickers
-        | 
+        |
         o-mars *
-"""  # noqa: E122, W291
+"""  # noqa: E122
         )
-        assert_command(['status'], expected_status_output, strip_indentation=False)
+        assert_command(['status'], expected_status_output.replace('|', '| '), strip_indentation=False)
