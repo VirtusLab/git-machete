2cd23fc333583dc254e443b84e7a9049ae233ec3
Pawel Lipski <pawel.p.lipski@gmail.com>
2022-07-26 18:35:59 +0200

    Add all existing png/svg versions of logo

--- ci/checks/prohibit-tab-character.sh
+++ ci/checks/prohibit-tab-character.sh
@@ -2,7 +2,7 @@
 
 set -e -o pipefail -u
 
-if git grep -In $'\t' -- ':!debian/rules'; then
+if git grep -In $'\t' -- ':!debian/rules' ':!*.svg'; then
   echo 'The above lines contain tab character (instead of spaces), please tidy up'
   exit 1
 fi
