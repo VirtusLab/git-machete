d6504202b52eecbc1601a912b6aa57aaae5eece2
Andrzej Malota <andrzej.malota@tesco.com>
2022-08-16 17:06:27 +0200

    cd

--- git_machete/client.py
+++ git_machete/client.py
@@ -68,7 +68,7 @@ class MacheteClient:
     def up_branch(self, val: Dict[LocalBranchShortName, Optional[LocalBranchShortName]]) -> None:
         self._up_branch = val
 
-    def get_childless_branches(self) -> List[str]:
+    def get_childless_branches(self) -> List[LocalBranchShortName]:
         parent_branches = [parent_branch for parent_branch, child_branches in self.__down_branches.items() if len(child_branches) > 0]
         return excluding(self.managed_branches, parent_branches)
 
