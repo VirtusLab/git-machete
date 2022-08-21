5e1d050805348c715861c0051c718251c789d759
Pawel Lipski <pawel.p.lipski@gmail.com>
2022-08-21 18:17:01 +0200

    Rectify `ci/nixpkgs-build` and `ci/deb-ppa-test-install`

--- ci/nixpkgs-build/local-run.sh
+++ ci/nixpkgs-build/local-run.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 
-set -e -o pipefail -u
-
-cd "$(git rev-parse --show-toplevel)"/ci/nixpkgs-build/
+set -e -o pipefail -u -x
 
 source "$(git rev-parse --show-toplevel)"/ci/local-run-commons.sh nixpkgs-build
+
 export_directory_hash nixpkgs-build
+cd "$(git rev-parse --show-toplevel)"/ci/nixpkgs-build/
 
 git_revision=$(git rev-parse "@{upstream}")
-
 docker-compose build nixpkgs-build
-docker-compose run -e GIT_REVISION=$git_revision nixpkgs-build
+docker-compose run -e GIT_REVISION="$git_revision" nixpkgs-build
