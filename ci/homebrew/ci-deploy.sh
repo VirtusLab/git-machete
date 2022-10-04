7be06bfe880d1af4e30b7d8ad1d7782e22dd0f41
amalota <amalota@virtuslab.com>
2022-10-04 15:57:51 +0200

    init

--- ci/homebrew/ci-deploy.sh
+++ ci/homebrew/ci-deploy.sh
@@ -2,6 +2,9 @@
 
 set -e -o pipefail -u
 
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" < /dev/null
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
 if [[ ${1-} == "--dry-run" || ${CIRCLE_BRANCH-} != "master" ]]; then
   do_push=false
   pypi_host=test.pypi.org
@@ -17,7 +20,7 @@ sha256=$(
   | jq --raw-output '.urls | map(select(.packagetype == "sdist")) | .[0].digests.sha256')
 
 if [[ $do_push == true ]]; then
-  brew brew bump-formula-pr --url $url --sha256 $sha256 git-machete
+  brew bump-formula-pr --url $url --sha256 $sha256 git-machete
 else
   echo "Refraining from push since it's a dry run"
   brew bump-formula-pr --dry-run --url $url --sha256 $sha256 git-machete
