684f003afc568e7c8fb41545dc41408ed940bffe
amalota <amalota@virtuslab.com>
2022-10-28 13:07:33 +0200

    cd

--- ci/snap/ci-deploy.sh
+++ ci/snap/ci-deploy.sh
@@ -25,6 +25,8 @@ else
 #  echo "$SNAPCRAFT_LOGIN_CREDENTIALS_CONTENTS_BASE64" | base64 -d > ~/.snapcraft.login
 #  cat /home/circleci/.cache/snapcraft/log/snapcraft-20220708-133250.692570.log
   export SNAPCRAFT_STORE_CREDENTIALS=$SNAPCRAFT_LOGIN_CREDENTIALS_CONTENTS_BASE64
+  echo $SNAPCRAFT_STORE_CREDENTIALS
+  snapcraft --version
   snapcraft login
   snapcraft whoami
   snapcraft upload --release=edge git-machete*.snap
