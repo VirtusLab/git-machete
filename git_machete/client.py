7c2dc4b38f3264ee19b92b93f93885a9c16e7be2
amalota <amalota@virtuslab.com>
2022-11-24 14:26:25 +0100

    fix

--- git_machete/client.py
+++ git_machete/client.py
@@ -1430,10 +1430,9 @@ class MacheteClient:
         return result
 
     def sync_annotations_to_github_prs(self) -> None:
-        remote, org, repo = self.__derive_remote_and_github_org_and_repo()
+        _, org, repo = self.__derive_remote_and_github_org_and_repo()
         current_user: Optional[str] = git_machete.github.derive_current_user_login()
         debug('Current GitHub user is ' + (current_user or '<none>'))
-        pr: GitHubPullRequest
         all_prs: List[GitHubPullRequest] = derive_pull_requests(org, repo)
         self.__sync_annotations_to_definition_file(all_prs, current_user)
 
