4a8ac78c46a46db870a7c717e7fa4216e187e86c
Pawel Lipski <pawel.p.lipski@gmail.com>
2023-06-12 15:29:01 +0200

    Tweak lint-no-dead-urls options

--- ci/checks/enforce-links-correct.sh
+++ ci/checks/enforce-links-correct.sh
@@ -9,4 +9,6 @@ else
   rc_path=.remarkrc-allow-dead-urls.yml
 fi
 
-remark --frail --ignore-path=.gitignore --rc-path=$rc_path .
+# This check has a relatively low importance, and turned out to be very flaky.
+# Let's make it non-blocking.
+remark --frail --ignore-path=.gitignore --rc-path=$rc_path . || true
