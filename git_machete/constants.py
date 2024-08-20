c463560b34027cfae91a80b561ce223a0162d428
Pawel Lipski <pawel.p.lipski@gmail.com>
2024-08-20 14:29:54 +0200

    wip

--- git_machete/constants.py
+++ git_machete/constants.py
@@ -1,5 +1,5 @@
 from enum import Enum
-from typing import Optional
+from typing import Optional, Type, TypeVar
 
 from git_machete.exceptions import MacheteException
 
@@ -8,17 +8,21 @@ MAX_COMMITS_FOR_SQUASH_MERGE_DETECTION = 1000
 INITIAL_COMMIT_COUNT_FOR_LOG = 10
 TOTAL_COMMIT_COUNT_FOR_LOG = 100
 
+E = TypeVar('E', bound='Enum')
 
-class SquashMergeDetection(Enum):
+
+class ParsableEnum(Enum):
+    @classmethod
+    def from_string(cls: Type[E], value: str, from_where: Optional[str]) -> E:
+        try:
+            return cls[value.upper()]
+        except KeyError:
+            valid_values = ', '.join(e.value for e in cls)
+            prefix = f"Invalid value for {from_where}" if from_where else "Invalid value"
+            raise MacheteException(f"{prefix}: `{value}`. Valid values are `{valid_values}`")
+
+
+class SquashMergeDetection(ParsableEnum):
     NONE = "none"
     SIMPLE = "simple"
     EXACT = "exact"
-
-    @staticmethod
-    def from_string(value: str, from_where: Optional[str]) -> 'SquashMergeDetection':
-        try:
-            return SquashMergeDetection[value.upper()]
-        except KeyError:
-            valid_values = ', '.join(e.value for e in SquashMergeDetection)
-            prefix = f"Invalid value for {from_where}" if from_where else "Invalid value"
-            raise MacheteException(f"{prefix}: `{value}`. Valid values are `{valid_values}`")
