b5a0070355522d7a6ba6be38d4c0a3b95c761dc8
Pawel Lipski <pawel.p.lipski@gmail.com>
2024-09-18 17:44:56 +0200

    Fix 400 Bad Request in Test PyPI uploads

--- ci/pypi/ci-deploy.sh
+++ ci/pypi/ci-deploy.sh
@@ -5,6 +5,18 @@ set -e -o pipefail -u
 if [[ ${1-} == "--dry-run" || ${CIRCLE_BRANCH-} != "master" ]]; then
   repository=testpypi
   token=$TEST_PYPI_TOKEN
+  set -x
+  current_version=$(cut -d\' -f2 git_machete/__init__.py)
+  latest_pypi_version=$(curl -s "https://test.pypi.org/pypi/git-machete/json" | jq -r '.releases | keys | .[]' | sort -V | tail -1)
+  if [[ ${latest_pypi_version} == "$current_version".post* ]]; then
+    ordinal=${latest_pypi_version#*.post}
+    ordinal=$((ordinal+1))
+  else
+    ordinal=1
+  fi
+  sed -i "s/'$/-$ordinal'/" git_machete/__init__.py
+  cat git_machete/__init__.py
+  set +x
 else
   repository=pypi
   token=$PYPI_TOKEN
@@ -19,4 +31,4 @@ EOF
 set -x
 pip3 install -r requirements/pypi-publish.txt
 python3 -m build
-python3 -m twine upload --repository $repository --skip-existing dist/*
+python3 -m twine upload --repository $repository --skip-existing --verbose dist/*
