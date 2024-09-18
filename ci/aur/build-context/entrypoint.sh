08df355b759899a538eb55980a8a34df567c01e0
Pawel Lipski <pawel.p.lipski@gmail.com>
2024-09-18 17:40:29 +0200

    Simplify seds

--- ci/aur/build-context/entrypoint.sh
+++ ci/aur/build-context/entrypoint.sh
@@ -7,11 +7,10 @@ cd git-machete/
 
 sed -i "s/.*pytest  *-m.*/  pytest --numprocesses=auto -m 'not completion_e2e' -vv/" PKGBUILD
 sed -i "s/checkdepends=.*/checkdepends=('python-pytest' 'python-pytest-mock' 'python-pytest-xdist')/" PKGBUILD
-sed -i "s/pkgver=.*/pkgver=0.0.0/" PKGBUILD
+sed -i "s/pkgver=.*/pkgver=$GIT_REVISION/" PKGBUILD
 
 url=https://github.com/VirtusLab/git-machete/archive/$GIT_REVISION.tar.gz
 sed -i "s!::[^\"]*!::$url!" PKGBUILD
-sed -i "s!cd ..srcdir/.pkgname-..pkgver..!ls -l \$srcdir/; cd \$srcdir/git-machete-$GIT_REVISION!" PKGBUILD
 hash=$(curl -L -s $url | sha256sum | head -c 64)
 sed -i "s/sha256sums=.*/sha256sums=('$hash')/" PKGBUILD
 cat PKGBUILD
