2e92b9b70bf9169306cee022dc00fb257ba48b8a
Pawel Lipski <pawel.p.lipski@gmail.com>
2024-10-08 16:37:37 +0200

    Update multiple PR/MR descriptions in bulk (flags + subcommands)

--- git_machete/options.py
+++ git_machete/options.py
@@ -29,6 +29,7 @@ class CommandLineOptions:
         self.opt_override_to_parent: bool = False
         self.opt_push_tracked: bool = True
         self.opt_push_untracked: bool = True
+        self.opt_related: bool = False
         self.opt_removed_from_remote: bool = False
         self.opt_return_to: str = "stay"
         self.opt_roots: List[LocalBranchShortName] = list()
