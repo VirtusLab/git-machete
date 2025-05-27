3389de41d4e1a5730d4bc7f6e1fef2dd8239ecb5
Pawel Lipski <pawel.p.lipski@gmail.com>
2025-05-27 09:35:14 +0200

    Fix patching PKGBUILD file for AUR package

--- ci/aur/build-context/entrypoint.sh
+++ ci/aur/build-context/entrypoint.sh
@@ -9,7 +9,7 @@ sed -i "s/pkgver=.*/pkgver=$GIT_REVISION/" PKGBUILD
 url=https://github.com/VirtusLab/git-machete/archive/$GIT_REVISION.tar.gz
 sed -i "s!::[^\"]*!::$url!" PKGBUILD
 hash=$(curl -L -s $url | sha256sum | head -c 64)
-sed -i "s/sha256sums=.*/sha256sums=('$hash')/" PKGBUILD
+sed -i "s/sha256sums=('[^']*/sha256sums=('$hash/" PKGBUILD
 cat PKGBUILD
 
 makepkg --syncdeps --log --install --clean --noconfirm
