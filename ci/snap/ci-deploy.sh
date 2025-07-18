877876c3f19e2efd92f718196b9717a2c579b5e9
Pawel Lipski <pawel.p.lipski@gmail.com>
2025-07-18 18:14:57 +0200

    Publish snaps for arm64 as well

--- ci/snap/ci-deploy.sh
+++ ci/snap/ci-deploy.sh
@@ -2,12 +2,6 @@
 
 set -e -o pipefail -u -x
 
-# By default, CircleCi VM's terminal width and height equals 0 which causes `craft_cli/messages.py`, line 279 to exit with `ZeroDivisionError`.
-# The terminal width in `craft_cli/messages.py` is being retrieved by `shutil.get_terminal_size()` using `COLUMNS` env var on line 1365
-# from `https://github.com/python/cpython/blob/3.10/Lib/shutil.py`, which can be easily fixed by setting `COLUMNS` env var value to some positive number.
-# Reference to the issue: https://github.com/canonical/craft-cli/issues/85
-export COLUMNS=1
-
 sudo apt-get update
 sudo apt-get install -y snapd
 sudo snap install review-tools
@@ -20,18 +14,23 @@ sudo iptables -A FORWARD -i lxdbr0 -j ACCEPT
 sudo iptables -A FORWARD -o lxdbr0 -j ACCEPT
 # `--use-lxd` applied to use a LXD container instead of a VM, to work around lack of support for KVM on CircleCI VMs.
 snapcraft --use-lxd
+ls -l -- *.snap
 
 if [[ ${1-} == "--dry-run" || ${CIRCLE_BRANCH-} != "master" ]]; then
   if command -v git-machete; then exit 1; fi
-  sudo snap install git-machete*.snap --dangerous --classic
+  sudo snap install git-machete_*_amd64.snap --dangerous --classic
   git machete version
   if ! git machete completion bash | grep '#!.*bash'; then
     echo "shell completion is not available in runtime, aborting"
     exit 1
   fi
   sudo snap remove git-machete
+
+  snapcraft upload --release=edge git-machete_*_amd64.snap
+  snapcraft upload --release=edge git-machete_*_arm64.snap
 else
   # Relying on SNAPCRAFT_STORE_CREDENTIALS, provided by the CI
-  snapcraft upload --release=stable git-machete*.snap
+  snapcraft upload --release=stable git-machete_*_amd64.snap
+  snapcraft upload --release=stable git-machete_*_arm64.snap
   snapcraft status git-machete
 fi
