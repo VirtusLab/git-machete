710cbbf221b73cf0733a22b5fa8cefd5b42b5cc3
Pawel Lipski <pawel.p.lipski@gmail.com>
2025-10-10 14:06:13 +0200

    Bump AUR image

--- ci/aur/build-context/entrypoint.sh
+++ ci/aur/build-context/entrypoint.sh
@@ -5,7 +5,7 @@ set -e -o pipefail -u
 git clone https://aur.archlinux.org/git-machete.git
 cd git-machete
 
-sed -i "s/pkgver=.*/pkgver=$GIT_REVISION/" PKGBUILD
+sed -i "s/pkgver=.*/pkgver=${GIT_REVISION}/" PKGBUILD
 url=https://github.com/VirtusLab/git-machete/archive/${GIT_REVISION}.tar.gz
 sed -i "s!::[^\"]*!::$url!" PKGBUILD
 hash=$(curl -L -s $url | sha256sum | head -c 64)
