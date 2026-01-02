cb11ef9b22a3bc6cd616ac5f5c4559191c7cd02b
Pawel Lipski <pawel.p.lipski@gmail.com>
2026-01-02 16:17:50 +0100

    Try fixing cacerts + tag path

--- ci/nixpkgs/build-context/entrypoint.sh
+++ ci/nixpkgs/build-context/entrypoint.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
 set -e -o pipefail
 
-# Required Env Vars:
-# GIT_REVISION: The version/tag you want to test (e.g., "3.38.1")
-# EXPRESSION_PATH: Path inside nixpkgs (e.g., "pkgs/by-name/gi/git-machete/package.nix")
+# GIT_REVISION: Can be a tag (3.38.1) or a hash (874931f...)
+# EXPRESSION_PATH: pkgs/by-name/gi/git-machete/package.nix
 
 echo "--- Cloning nixpkgs ---"
 git clone --depth=1 https://github.com/NixOS/nixpkgs.git .
 
-echo "--- Updating $EXPRESSION_PATH to version $GIT_REVISION ---"
-# nix-update will:
-# 1. Modify the version string in the .nix file
-# 2. Fetch the source for that version
-# 3. Calculate the correct hash
-# 4. Replace the old hash in the .nix file
-# 5. Run the build to verify everything
+echo "--- Pre-patching expression to support raw revisions ---"
+# We change 'tag = "v${version}"' to 'rev = version'
+# This makes the expression compatible with both version numbers and commit hashes.
+sed -i 's/tag = "v\${version}"/rev = version/' "$EXPRESSION_PATH"
+
+echo "--- Updating $EXPRESSION_PATH to $GIT_REVISION ---"
+# --version will now set the 'version' variable to your hash/tag.
+# Since we changed the .nix file to use 'rev = version',
+# the URL will now correctly point to the raw revision.
 nix-update git-machete --version "$GIT_REVISION" --build
 
 echo "--- Build and Tests Successful! ---"
-# Show the modified file so you can see the clean update
 cat "$EXPRESSION_PATH"
