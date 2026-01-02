c3a56ccdb1f866a551b223993ea7140388ef8633
Pawel Lipski <pawel.p.lipski@gmail.com>
2026-01-02 16:34:07 +0100

    Fix changelog

--- ci/nixpkgs/build-context/entrypoint.sh
+++ ci/nixpkgs/build-context/entrypoint.sh
@@ -1,21 +1,24 @@
 #!/usr/bin/env bash
 set -e -o pipefail
 
-# GIT_REVISION: Can be a tag (3.38.1) or a hash (874931f...)
+# GIT_REVISION: Tag (e.g. 3.38.1) or Hash (e.g. 874931f)
 # EXPRESSION_PATH: pkgs/by-name/gi/git-machete/package.nix
 
 echo "--- Cloning nixpkgs ---"
 git clone --depth=1 https://github.com/NixOS/nixpkgs.git .
 
-echo "--- Pre-patching expression to support raw revisions ---"
-# We change 'tag = "v${version}"' to 'rev = version'
-# This makes the expression compatible with both version numbers and commit hashes.
+echo "--- Pre-patching expression ---"
+
+# 1. Support raw revisions (tags or hashes) in the fetcher
 sed -i 's/tag = "v\${version}"/rev = version/' "$EXPRESSION_PATH"
 
+# 2. Fix the changelog URL
+# We replace the broken ${src.tag} with v${version}
+# (Most GitHub releases use the 'v' prefix in the URL even if the revision is a hash/tag)
+sed -i 's/\${src.tag}/v\${version}/' "$EXPRESSION_PATH"
+
 echo "--- Updating $EXPRESSION_PATH to $GIT_REVISION ---"
-# --version will now set the 'version' variable to your hash/tag.
-# Since we changed the .nix file to use 'rev = version',
-# the URL will now correctly point to the raw revision.
+# nix-update will now set 'version' and 'hash' correctly.
 nix-update git-machete --version "$GIT_REVISION" --build
 
 echo "--- Build and Tests Successful! ---"
