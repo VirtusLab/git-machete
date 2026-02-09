6417e5cded08271ed21629c0ff7d159d6961a5d0
Pawel Lipski <pawel.p.lipski@gmail.com>
2026-02-09 11:55:39 +0100

    Update nixpkgs fix to work after NixOS/nixpkgs#487735

--- ci/nixpkgs/build-context/entrypoint.sh
+++ ci/nixpkgs/build-context/entrypoint.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
-set -e -o pipefail -u
+set -e -o pipefail -u -x
 
-# GIT_REVISION: Tag (e.g. v3.38.1) or Hash (e.g. 874931f)
-# EXPRESSION_PATH: path to git-machete expression within NixOS/nixpkgs
+# GIT_REVISION: tag (e.g. v3.38.1) or hash (e.g. 874931f)
+
+EXPRESSION_PATH=pkgs/by-name/gi/git-machete/package.nix
 
 git clone --depth=1 https://github.com/NixOS/nixpkgs.git .
 
-sed -i 's/tag = "v\${version}"/rev = version/' "$EXPRESSION_PATH"
+sed -i 's/tag = "v\${.*version}"/rev = finalAttrs.version/' "$EXPRESSION_PATH"
 
 # Remove changelog URL as it contains ${src.tag} which is undefined after the sed above
 sed -i '/changelog = / d' "$EXPRESSION_PATH"
@@ -16,7 +17,7 @@ sed -i '/changelog = / d' "$EXPRESSION_PATH"
 # but when we're testing a commit hash, the actual version in the code won't match.
 sed -i '/postInstallCheck = /, /'';$/ d' "$EXPRESSION_PATH"
 
-cat "$EXPRESSION_PATH"
+git diff "$EXPRESSION_PATH" | cat
 
 # nix-update will now set 'version' and 'hash' correctly, and also run the tests.
 nix-update git-machete --version "$GIT_REVISION" --build
