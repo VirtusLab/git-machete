b02b9d0c0a363698a25d2a00b3942bdfbe8955b1
Paweł Lipski <pawel.p.lipski@gmail.com>
2026-09-09 12:51:31 +0200

    Add rationale comment to the UTF Flake8 open-encoding check (#1781)
    Add rationale comment to the UTF Flake8 check explaining why text files must be opened as UTF-8.

--- flake8/utf8_open_checker.py
+++ flake8/utf8_open_checker.py
@@ -4,14 +4,10 @@ from typing import Optional
 from flake8_plugin_utils import Error, Plugin, Visitor
 
 
+# Python otherwise uses the locale-dependent default encoding, which can make files written on Windows unreadable as UTF-8 elsewhere.
 class TextFileShouldBeOpenedAsUtf8(Error):
     code = "UTF100"
     message = "Text files must be opened with encoding='utf-8'"
-
-
-class TextFileWriteShouldPinNewline(Error):
-    code = "UTF101"
-    message = "Text files opened for writing must set newline='\\n' or newline=''"
 
 
 class Utf8OpenVisitor(Visitor):
@@ -50,28 +46,14 @@ class Utf8OpenVisitor(Visitor):
         return mode_value is not None and "b" in mode_value
 
     @classmethod
-    def _is_write_mode(cls, node: ast.Call) -> bool:
-        mode_value = cls._mode_value(node)
-        return mode_value is not None and any(flag in mode_value for flag in "wax+")
-
-    @classmethod
     def _is_utf8_encoding(cls, node: ast.Call) -> bool:
         encoding = node.args[3] if len(node.args) > 3 else cls._keyword_value(node, "encoding")
         encoding_value = cls._string_value(encoding)
         return encoding_value is not None and encoding_value.lower().replace("_", "-") == "utf-8"
 
-    @classmethod
-    def _has_pinned_newline(cls, node: ast.Call) -> bool:
-        newline = node.args[5] if len(node.args) > 5 else cls._keyword_value(node, "newline")
-        newline_value = cls._string_value(newline)
-        return newline_value in ("\n", "")
-
     def visit_Call(self, node: ast.Call) -> None:
-        if self._is_open_call(node) and not self._is_binary_mode(node):
-            if not self._is_utf8_encoding(node):
-                self.error_from_node(TextFileShouldBeOpenedAsUtf8, node)
-            if self._is_write_mode(node) and not self._has_pinned_newline(node):
-                self.error_from_node(TextFileWriteShouldPinNewline, node)
+        if self._is_open_call(node) and not self._is_binary_mode(node) and not self._is_utf8_encoding(node):
+            self.error_from_node(TextFileShouldBeOpenedAsUtf8, node)
         self.generic_visit(node)
 
 
